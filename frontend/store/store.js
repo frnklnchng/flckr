6fe61731d2acd432254f90bd31552187c69ce12d
Franklin Chang <frnklnchng@gmail.com>
2018-09-17 16:45:43 -0700

    Reenable redux-logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -7,8 +7,8 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    // applyMiddleware(thunk, logger)
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
+    // applyMiddleware(thunk)
   )
 );
 
