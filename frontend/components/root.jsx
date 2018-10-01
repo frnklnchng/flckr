78c450120e1e71d9a328e18e99177ea44b24588a
Franklin Chang <frnklnchng@gmail.com>
2018-10-01 13:50:23 -0700

    Attempt scroll management

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { ScrollContext } from 'react-router-scroll-4';
 import App from './App';
 
 const Root = ({ store }) => (
   <Provider store={ store }>
     <HashRouter>
-      <App />
+      <ScrollContext>
+        <App />
+      </ScrollContext>
     </HashRouter>
   </Provider>
 );
